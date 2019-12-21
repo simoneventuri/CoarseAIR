07e9a2cf29981136c22351db8a294811e8357860
Simone Venturi <sim.venturi@gmail.com>
2019-12-21 14:12:58 +0100

    Modified Python Postprocessing

--- scripts/postprocessing/ExtCode_PipeLine/Initialize_0D_Database_Function.sh
+++ scripts/postprocessing/ExtCode_PipeLine/Initialize_0D_Database_Function.sh
@@ -18,45 +18,45 @@
 # 
 #---------------------------------------------------------------------------------------------------------------
 #===============================================================================================================
-function Initialize_0D_Database{
+function Initialize_0D_Database {
 # Ex. of Call: bash Initialize_0D_Database.sh O3 10000 $WORKSPACE_PATH/Mars_Database/Run_0D/database/kinetics/ 1 1 1
 
-	#System=${1}
-	#echo "  [Initialize_0D_Database]: System                = "$System
-	#TTran=${2}
-	#echo "  [Initialize_0D_Database]: Transl. T             = "$TTran
-	#PathToDtbFldr=${3}
-	#PathToDtbFldr=$WORKSPACE_PATH/Mars_Database/Run_0D/database/kinetics/
-	#echo "  [Initialize_0D_Database]: Path To Kinetic Fldr? = "$PathToDtbFldr
-	#DissFlg=${4}
-	#echo "  [Initialize_0D_Database]: Writing Dissociation? = "$DissFlg
-	#InelFlg=${5}
-	#echo "  [Initialize_0D_Database]: Writing Inelastic?    = "$InelFlg
-	#ExchFlg=${6}
-	#echo "  [Initialize_0D_Database]: How Many Exchanges?   = "$ExchFlg
+#System=${1}
+#echo "  [Initialize_0D_Database]: System                = "$System
+#TTran=${2}
+#echo "  [Initialize_0D_Database]: Transl. T             = "$TTran
+#PathToDtbFldr=${3}
+#PathToDtbFldr=$WORKSPACE_PATH/Mars_Database/Run_0D/database/kinetics/
+#echo "  [Initialize_0D_Database]: Path To Kinetic Fldr? = "$PathToDtbFldr
+#DissFlg=${4}
+#echo "  [Initialize_0D_Database]: Writing Dissociation? = "$DissFlg
+#InelFlg=${5}
+#echo "  [Initialize_0D_Database]: Writing Inelastic?    = "$InelFlg
+#ExchFlg=${6}
+#echo "  [Initialize_0D_Database]: How Many Exchanges?   = "$ExchFlg
 
 
-	echo 'Units=cm^3/s' > $PathToDtbFldr/'/kinetics/KineticsTEMP'
+echo "Units=cm^3/s" > $PathToDtbFldr"/kinetics/KineticsTEMP"
 
-	iDiss=1
-	while [ ${iDiss} -le ${DissFlg} ]; do 
-		echo "  [Initialize_0D_Database]: Adding Dissociation Kinetics to File "$PathToDtbFldr/'/kinetics/KineticsTEMP'
-		cat $PathToDtbFldr/$System'Diss_'$TTran'K.dat' >> $PathToDtbFldr/'/kinetics/KineticsTEMP'
-		iDiss=$((iDiss+1))
-	done
+iDiss=1
+while [ ${iDiss} -le ${DissFlg} ]; do 
+	echo "  [Initialize_0D_Database]: Adding Dissociation Kinetics to File "$PathToDtbFldr/"/kinetics/KineticsTEMP"
+	cat $PathToDtbFldr/$System"Diss_"$TTran"K.dat" >> $PathToDtbFldr/"/kinetics/KineticsTEMP"
+	iDiss=$((iDiss+1))
+done
 
-	iInel=1
-	while [ ${iInel} -le ${InelFlg} ]; do 
-		echo "  [Initialize_0D_Database]: Adding Inelastic Kinetics to File "$PathToDtbFldr/'/kinetics/KineticsTEMP'
-		cat $PathToDtbFldr/$System'Inel_'$TTran'K.dat' >> $PathToDtbFldr/'/kinetics/KineticsTEMP'
-		iInel=$((iInel+1))
-	done
+iInel=1
+while [ ${iInel} -le ${InelFlg} ]; do 
+	echo "  [Initialize_0D_Database]: Adding Inelastic Kinetics to File "$PathToDtbFldr/"/kinetics/KineticsTEMP"
+	cat $PathToDtbFldr/$System"Inel_"$TTran"K.dat" >> $PathToDtbFldr/"/kinetics/KineticsTEMP"
+	iInel=$((iInel+1))
+done
 
-	iExch=1
-	while [ ${iExch} -le ${InelFlg} ]; do 
-		echo "  [Initialize_0D_Database]: Adding Exchange Kinetics to File "$PathToDtbFldr/'/kinetics/KineticsTEMP'
-		cat $PathToDtbFldr/$System'Exch_Type'$iExch'_'$TTran'K.dat' >> $PathToDtbFldr/'/kinetics/KineticsTEMP'
-		iExch=$((iExch+1))
-	done
+iExch=1
+while [ ${iExch} -le ${InelFlg} ]; do 
+	echo "  [Initialize_0D_Database]: Adding Exchange Kinetics to File "$PathToDtbFldr/"/kinetics/KineticsTEMP"
+	cat $PathToDtbFldr/$System"Exch_Type"$iExch"_"$TTran"K.dat" >> $PathToDtbFldr/"/kinetics/KineticsTEMP"
+	iExch=$((iExch+1))
+done
 
 }
