c927c9ee9e7ea8b49e70f9d9c328d5627e28d6a0
Simone Venturi <sim.venturi@gmail.com>
2020-02-26 21:39:42 -0600

    Modified for running CG-QCT with 4 Atoms system

--- scripts/executing/ComputeRates.sh
+++ scripts/executing/ComputeRates.sh
@@ -71,7 +71,7 @@ function ComputeTrajsPBS {
         if [ "${ProcType}" = "none" ]; then
           sed -e '3s/$/1:ppn='${NProc}'/'                           'RunTrajectories-Format-UIUC.pbs'     > 'RunTrajectoriesTEMP-1.pbs'
         if [ "${ProcType}" = "test" ]; then
-          sed -e '3s/$/1:ncpus='${NProc}':model='${ProcType}'/'     'RunTrajectories-Format-Test.pbs'     > 'RunTrajectoriesTEMP-1.pbs'
+          sed -e '3s/$/1:ncpus='${NProc}':model=ivy/'               'RunTrajectories-Format-Test.pbs'     > 'RunTrajectoriesTEMP-1.pbs'
         else
           sed -e '3s/$/1:ncpus='${NProc}':model='${ProcType}'/'     'RunTrajectories-Format-Pleiades.pbs' > 'RunTrajectoriesTEMP-1.pbs'
         fi
@@ -555,6 +555,8 @@ function PostTrajectoriesPBS {
   cd ${COARSEAIR_OUTPUT_DIR}/..
   if [ "${ProcType}" = "none" ]; then
     scp ${COARSEAIR_SH_DIR}/PostTrajectories-Format-UIUC.pbs     ./
+  elif [ "${ProcType}" = "test" ]; then
+    scp ${COARSEAIR_SH_DIR}/PostTrajectories-Format-Test.pbs     ./
   else
     scp ${COARSEAIR_SH_DIR}/PostTrajectories-Format-Pleiades.pbs ./
   fi
@@ -669,7 +671,7 @@ function PostTrajectoriesPBS {
               if [ "${ProcType}" = "none" ]; then
                 sed -e '3s/$/1:ppn='${NProc}'/'                        'PostTrajectories-Format-UIUC.pbs'     > 'PostTrajectoriesTEMP-1.pbs'
               elif [ "${ProcType}" = "test" ]; then
-                sed -e '3s/$/1:ncpus='${NProc}':model='${ProcType}'/'  'PostTrajectories-Format-Test.pbs'     > 'PostTrajectoriesTEMP-1.pbs'
+                sed -e '3s/$/1:ncpus='${NProc}':model=ivy/'            'PostTrajectories-Format-Test.pbs'     > 'PostTrajectoriesTEMP-1.pbs'
               else
                 sed -e '3s/$/1:ncpus='${NProc}':model='${ProcType}'/'  'PostTrajectories-Format-Pleiades.pbs' > 'PostTrajectoriesTEMP-1.pbs'
               fi
